5b749ad022529237a09f57185bacbcaf8c8993f0
pavelkalinchuk <715m4s@gmail.com>
2025-01-08 23:22:22 +0300

    Добавлены настройки для отправления отчёта в телеграм канал

--- tests/test_check_random.py
+++ tests/test_check_random.py
@@ -6,6 +6,6 @@ import allure
 def test_check_random():
     with allure.step("Генерируем рандомное число от 1 до 10"):
         random_integer = random.randint(1, 10)
-    with allure.step("Проверяем сгенерированное число на чёт нечёт"):
+    with allure.step("Проверяем сгенерированное число на чёт/нечёт"):
         assert random_integer % 2 == 0, f"Проигрыш, выпал нечёт ({random_integer})"
 
